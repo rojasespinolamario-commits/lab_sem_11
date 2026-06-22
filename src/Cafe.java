8e8d8425b47da2b42c457cd8fea8445f4fed5f16
rojasespinolamario <rojasespinolamario@gmail.com>
2026-06-21 19:13:35 -0500

    refactor: agregue atributos de tamaño a la clase Cafe

--- src/Cafe.java
+++ src/Cafe.java
@@ -1,11 +1,19 @@
 public abstract class Cafe {
 
     String descripcion = "";
+    String tamaño = Tamaño.N;
 
     public String getDescripcion() {
         return descripcion;
     }
 
-    public abstract double costo();
+    public String getTamaño() {
+        return tamaño;
+    }
 
+    public void setTamaño(String tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public abstract double costo();
 }
