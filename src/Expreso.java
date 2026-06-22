568c1d0660d0b8e170d72ec1b3be88b49c9f8020
rojasespinolamario <rojasespinolamario@gmail.com>
2026-06-21 19:19:14 -0500

    refactor: implemente precios por tamaño para la clase Expreso usando HashMap

--- src/Expreso.java
+++ src/Expreso.java
@@ -1,10 +1,21 @@
+import java.util.HashMap;
+
 public class Expreso extends Cafe {
+
+    private static final HashMap<String, Double> precios = new HashMap<>();
+
+    static {
+        precios.put(Tamaño.N, 1.99);
+        precios.put(Tamaño.M, 2.09);
+        precios.put(Tamaño.G, 2.15);
+    }
 
     public Expreso () {
         descripcion = "Cafe expreso";
     }
 
+    @Override
     public double costo() {
-        return 1.99;
+        return precios.get(tamaño);
     }
 }
